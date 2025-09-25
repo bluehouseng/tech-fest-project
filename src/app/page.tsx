90104a590859901ec2be4ea7b0bba7f5beedee36
shalom bamshak <bamshakcheson@gmail.com>
2025-09-25 15:21:41 +0100

    i worked on host and speaker section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,20 @@ import Body from "./components/landing/body";
 import Agenda from "./components/landing/agenda-review";
 import Partners from "./components/landing/partners";
 import Footer from "./components/landing/footer";
+import Host from "./components/landing/host";
+import Keyspeaker from "./components/landing/keySpeakers";
 
 export default function LandingPage() {
   return (
 	  <main>
 		<Header />
 		<Body />
+		<Host/>
+		<Keyspeaker/>
 		<Agenda />
 		<Partners />
 		<Footer />
+		
 	  </main>
   );
 }
